ecac3a2e9e1323fb91a1cf7258fcb368f90d047b
Aboubacar KOUMARE <elbacar08@gmail.com>
2020-03-09 21:01:43 +0100

    chore : change database access

--- src/main/java/com/etna/gpe/dto/OrganizationDto.java
+++ src/main/java/com/etna/gpe/dto/OrganizationDto.java
@@ -1,10 +1,5 @@
 package com.etna.gpe.dto;
 
-import javax.persistence.Column;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.validation.constraints.NotBlank;
 
 import org.springframework.lang.NonNull;
 
