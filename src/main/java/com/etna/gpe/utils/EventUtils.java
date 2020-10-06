e6a33f2b5073c982fa0b2cacfc1c9280af29c503
Aboubacar KOUMARE <elbacar08@gmail.com>
2020-10-06 05:39:08 +0200

    fix(event) : search event

--- src/main/java/com/etna/gpe/utils/EventUtils.java
+++ src/main/java/com/etna/gpe/utils/EventUtils.java
@@ -3,18 +3,13 @@ package com.etna.gpe.utils;
 public class EventUtils {
 	
 	private static String SPACE_STRING = " ";
-	private static String COMA_PLUS_SPACE_STRING = ", ";
 	private static String COMA_STRING = ",";
 	private static String EMPTY_STRING = "";
 
 	public static boolean isLocation(String locateRequested, String locate) {
 		
-		boolean isfound = locateRequested.equalsIgnoreCase(
-				getCountryOnLocation(locate).equals(EMPTY_STRING) ?
-				getTownOnLocation(locate) : 	
-				getTownOnLocation(locate) + COMA_PLUS_SPACE_STRING + getCountryOnLocation(locate));
+		boolean isfound = locateRequested.equalsIgnoreCase(getTownOnLocation(locate));
 		System.err.println("locateRequested " + locateRequested);
-		System.err.println("locate " + getTownOnLocation(locate) + COMA_PLUS_SPACE_STRING + getCountryOnLocation(locate));
 		return isfound;
 	}
 	
@@ -26,15 +21,5 @@ public class EventUtils {
 			town = locationSplit[length - 2];
 		}
 		return town.replace(COMA_STRING, EMPTY_STRING);
-	}
-	
-	private static String getCountryOnLocation(String location) {
-		String country = EMPTY_STRING;
-		String[] locationSplit = location.split(COMA_PLUS_SPACE_STRING);
-		int length = locationSplit.length;
-		if(length > 1) {
-			country = locationSplit[length - 1];
-		}
-		return country;
 	}
 }
